af208dff8c5bf83eecc4173d7c1e5ea8a6bbff3f
BaoXiangguai <1720528424@qq.com>
2024-06-10 13:44:42 +0800

    C++ 2024/6/10

---
+++ lessons/22.5 重载赋值运算符/ConsoleApplication1.cpp
@@ -0,0 +1,65 @@
+﻿/*
+
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class CGirl
+{
+	friend ostream& operator<< (ostream& cout, const CGirl& g);
+public:
+	int		m_bh;
+	string	m_name;
+	int* m_ptr;
+
+	CGirl() { m_ptr = nullptr; }
+	~CGirl() { if (m_ptr) delete m_ptr; }	// 释放内存空间
+
+	CGirl& operator=(const CGirl& g) {
+		if (this == &g) return *this;	// 防止g1=g1;这种情况重复操作
+		m_bh = g.m_bh; m_name = g.m_name;
+
+		if (g.m_ptr == nullptr) {
+			if (m_ptr != nullptr) { delete m_ptr; m_ptr = nullptr; }
+		}
+		else {
+			// 如果目标对象指针为空，先分配内存
+			if (m_ptr == nullptr) m_ptr = new int;
+			// 把源对象内存中的数据复制到目标对象的内存中
+			memcpy(m_ptr, g.m_ptr, sizeof(int));
+		}
+
+		return *this;	// 解引用，返回对象
+	}
+};
+
+ostream& operator<< (ostream& cout, const CGirl& g)		// 隐含了第一个参数：对象本身
+{
+	cout << "姓名：" << g.m_name << "，编号：" << g.m_bh
+		<< "，m_ptr=" << g.m_ptr << "，*m_ptr=" << *g.m_ptr;
+	return cout;
+}
+
+int main()
+{
+	CGirl g1, g2;
+	g1.m_bh = 8; g1.m_name = "西施"; g1.m_ptr = new int(3);
+	cout << g1 << endl;
+	g2 = g1;		// 对象赋值，多对多
+	cout << g2 << endl;
+}
+
+/*
+对象的赋值运算是用一个已经存在的对象，给另一个已经存在的对象赋值。
+如果类的定义中没有重载赋值函数，编译器就会提供一个默认赋值函数。
+如果类中重载了赋值函数，编译器将不提供默认赋值函数。
+重载赋值函数的语法:	类名 & operator=(const 类名 & 源对象);
+
+注意:
+编译器提供的默认赋值函数，是一种浅赋值行为。
+如果对象中不存在堆区内存空间，默认重载赋值函数可以满足需求，否则需要深复制。
+赋值运算和拷贝构造不同:拷贝构造是指原来的对象不存在，用已存在的对象进行构造;
+	赋值运算是指已经存在了两个对象，把其中一个对象的成员变量的值赋给另一个对象的成员变量。
+*/
---
+++ lessons/24.4 如何构造基类/ConsoleApplication1.cpp
@@ -0,0 +1,131 @@
+﻿/*
+
+*/
+
+#include <iostream>
+using namespace std;
+
+//class CAllComers		// 海选报名者类(基类)
+//{
+//protected:
+//	int m_a;
+//
+//private:
+//	int m_b;
+//
+//public:
+//	string m_name;
+//	string m_tel;
+//	CAllComers() { m_name = "XXX"; m_tel = "不详"; m_a = 1; m_b = 2; }
+//
+//	// 报名时需要唱一首歌
+//	void sing() { cout << "我是一只小小鸟。\n"; }
+//	void setname(const string name) { m_name = name; }
+//	void setTel(const string tel) { m_tel = tel; }
+//};
+//
+//class CGirl1 :public CAllComers		// 超女类(派生类)
+//{
+//public:
+//	int m_bh;
+//
+//	CGirl1() { m_bh = 8; }
+//
+//	void show() {
+//		cout << "编号：" << m_bh << "，姓名：" << m_name << "，联系方式：" << m_tel << endl;
+//	}
+//};
+//class CGirl2 :protected CAllComers		// 超女类(派生类)
+//{
+//public:
+//	int m_bh;
+//
+//	CGirl2() { m_bh = 8; }
+//
+//	void show() {
+//		cout << "编号：" << m_bh << "，姓名：" << m_name << "，联系方式：" << m_tel << endl;
+//	}
+//	void showvalue() { cout << m_a << endl; }
+//};
+//class CGirl3 :private CAllComers		// 超女类(派生类)
+//{
+//public:
+//	int m_bh;
+//
+//	CGirl3() { m_bh = 8; }
+//
+//	void show() {
+//		cout << "编号：" << m_bh << "，姓名：" << m_name << "，联系方式：" << m_tel << endl;
+//	}
+//	void showvalue() { cout << m_a << endl; }
+//};
+//
+////——————————————————————
+class A {
+public:
+	int m_a = 1;
+protected:
+	int m_b = 2;
+private:
+	int m_c = 3;	// 基类中的m_c对派生类而言只是隐藏了，但实际存在且占用空间
+
+public:
+	A() {
+		cout << "调用了A的构造函数。"
+			<< "\nA的this指针是：" << this
+			<< "\nA中m_a的地址是：" << &m_a
+			<< "\nA中m_b的地址是：" << &m_b
+			<< "\nA中m_c的地址是：" << &m_c << endl;
+	}
+	~A() { cout << "调用了A的析构函数。\n"; }
+
+	int using_m_c() { return m_c; }
+
+	void show() {
+		cout << "m_c=" << m_c << endl;
+	}
+};
+
+class B :public A {
+public:
+	int m_d = 40;
+
+	B() {
+		cout << "调用了B的构造函数。"
+			<< "\nB的this指针是：" << this
+			<< "\nB中m_a的地址是：" << &m_a
+			<< "\nB中m_b的地址是：" << &m_b
+			<< "\nB中m_d的地址是：" << &m_d << endl;;
+	}
+	~B() { cout << "调用了B的析构函数。\n"; }
+};
+
+// 重载 new 和 delete 操作符，用以跟踪对象在创建和销毁时，内存的分配和释放情况
+void* operator new(size_t size) {
+	void* ptr = malloc(size);
+	cout << "申请到的内存地址：" << ptr << "，大小是：" << size << endl;
+	return ptr;
+}
+void operator delete(void* ptr) {
+	if (ptr == 0) return;	// 对空指针delete是安全的
+	free(ptr);
+	cout << "释放了内存。\n";
+}
+
+int main()
+{
+	cout << "基类A占用的内存大小是：" << sizeof(A) << endl;
+	cout << "派生类B占用的内存大小是：" << sizeof(B) << endl;
+
+	B* b = new B;
+	delete b;
+
+	//———————奇技淫巧———————
+	B* p = new B;
+	*((int*)p + 2) = 99;	// 直接用指针访问基类私有成员
+	p->show();
+}
+
+/*
+
+*/
